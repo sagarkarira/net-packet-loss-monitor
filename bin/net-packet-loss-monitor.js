4c91551568928cf6c923867e45734bf6da24d8aa
sagar.karira <sagar.karira@mail.jugnoo.in>
2017-04-07 15:02:48 +0530

    changed logging interval

--- bin/net-packet-loss-monitor.js
+++ bin/net-packet-loss-monitor.js
@@ -1,6 +1,8 @@
+#!/usr/bin/env node
+
 'use strict';
 const path = require('path');
-const lib = require('../lib/app.js');
+const lib = require(path.join(__dirname, '../lib/app.js'));
 
 lib.main();
 setInterval(lib.main, lib.TIMEOUT_INTERVAL * 1000 * 60);
